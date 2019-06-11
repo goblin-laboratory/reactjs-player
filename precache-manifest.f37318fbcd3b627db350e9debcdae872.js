18e8c01e03e9d4889e422c4551b8d71912d59712
xiaosong <kunkkaco@gmail.com>
2019-06-11 09:24:58 +0800

    Updates

---
+++ precache-manifest.f37318fbcd3b627db350e9debcdae872.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "69e66b71ddf23f1deec3be4bb0041f0a",
+    "url": "/react-player/index.html"
+  },
+  {
+    "revision": "ddcb262c77e84d1df163",
+    "url": "/react-player/static/css/2.118e93a0.chunk.css"
+  },
+  {
+    "revision": "5cbb2b91c3ca700d5ab7",
+    "url": "/react-player/static/css/main.9dd10ab1.chunk.css"
+  },
+  {
+    "revision": "ddcb262c77e84d1df163",
+    "url": "/react-player/static/js/2.8074e04d.chunk.js"
+  },
+  {
+    "revision": "5cbb2b91c3ca700d5ab7",
+    "url": "/react-player/static/js/main.be9edf0f.chunk.js"
+  },
+  {
+    "revision": "40b7f6ee51a5714ac35c",
+    "url": "/react-player/static/js/runtime~main.094a6fdb.js"
+  },
+  {
+    "revision": "97d7e594697f0af4d4b9ec17772d234c",
+    "url": "/react-player/static/media/muted.97d7e594.svg"
+  },
+  {
+    "revision": "96cb285b92db787e9da9c5d2cb33dd56",
+    "url": "/react-player/static/media/unmuted.96cb285b.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.2a2bc01f92059aa5a4df924239409bfb.js"
+  "/react-player/precache-manifest.f37318fbcd3b627db350e9debcdae872.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/react-player/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
