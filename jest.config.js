522944238c7441056af5b3719b0e19d6e36a4cb1
xiaosong <kunkkaco@gmail.com>
2019-07-10 17:42:14 +0800

    fix(jest): 解决 import css 文件 jest 报错问题

--- jest.config.js
+++ jest.config.js
@@ -2,6 +2,11 @@ module.exports = {
   setupFilesAfterEnv: ['@testing-library/react/cleanup-after-each'],
   // setupFilesAfterEnv: ['<rootDir>/scripts/jest/setupTests.js'],
   modulePathIgnorePatterns: ['<rootDir>/example/'],
+  moduleNameMapper: {
+    // '\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$':
+    //   '<rootDir>/scripts/jest/fileMock.js',
+    '\\.(css|less)$': '<rootDir>/scripts/jest/styleMock.js',
+  },
   transform: {
     '^.+\\.(js|jsx|ts|tsx)$': 'babel-jest',
     '^.+\\.(css|less)$': '<rootDir>/scripts/jest/cssTransform.js',
