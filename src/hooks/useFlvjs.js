ad5ede19e3b474555916a7945a1afaa176135202
xiaosong <kunkkaco@gmail.com>
2019-08-07 18:13:22 +0800

    fix: src 变化时销毁 useFlv 的 kenerMsg

--- src/hooks/useFlvjs.js
+++ src/hooks/useFlvjs.js
@@ -1,33 +1,65 @@
 import React from 'react';
 import flvjs from 'flv.js';
+import utils from '../utils';
+
+const { debug, getElement } = utils;
 
 export default ({ src, config, onKernelError }, getVideoElement) => {
   const [flvPlayer, setFlvPlayer] = React.useState(null);
   const [kernelMsg, setKernelMsg] = React.useState(null);
 
+  const ref = React.useRef('');
+
+  const onFlvError = React.useCallback(
+    (type, detail) => {
+      const info = { type, detail };
+      setKernelMsg(info);
+      onKernelError(info);
+    },
+    [onKernelError],
+  );
+
   React.useEffect(() => {
-    const el = getVideoElement();
-    if (!el || !src) {
-      setFlvPlayer(null);
-      return () => {};
-    }
-    setFlvPlayer(flvjs.createPlayer({ isLive: true, type: 'flv', url: src }, { ...config }));
-    return () => {
-      setFlvPlayer(null);
+    const play = async () => {
+      if (!src) {
+        return;
+      }
+      const el = await getElement(getVideoElement);
+      if (ref.current !== src) {
+        return;
+      }
+      if (!el) {
+        ref.current = '';
+        // TODO: 提示用户
+        debug('useFlvjs: 获取 video 元素失败');
+        return;
+      }
+      setFlvPlayer(flvjs.createPlayer({ isLive: true, type: 'flv', url: src }, { ...config }));
     };
+
+    ref.current = src;
+    setFlvPlayer(null);
+    play();
+    return;
   }, [getVideoElement, src, config]);
 
   React.useEffect(() => {
-    if (!flvPlayer) {
+    const el = getVideoElement();
+    if (!flvPlayer || !el) {
       return () => {};
     }
-    const el = getVideoElement();
-    if (el) {
-      flvPlayer.attachMediaElement(el);
-      flvPlayer.load();
-      flvPlayer.play();
-    }
+    flvPlayer.on(flvjs.Events.ERROR, onFlvError);
+    flvPlayer.attachMediaElement(el);
+    flvPlayer.load();
+    // TODO: autoPlay
+    flvPlayer.play();
     return () => {
+      if (!flvPlayer) {
+        return;
+      }
+      try {
+        flvPlayer.off(flvjs.Events.ERROR, onFlvError);
+      } catch (errMsg) {}
       try {
         flvPlayer.pause();
       } catch (errMsg) {}
@@ -41,28 +73,11 @@ export default ({ src, config, onKernelError }, getVideoElement) => {
         flvPlayer.destroy();
       } catch (errMsg) {}
     };
-  }, [getVideoElement, flvPlayer]);
-
-  const onError = React.useCallback(
-    (type, detail) => {
-      const info = { type, detail };
-      setKernelMsg(info);
-      onKernelError(info);
-    },
-    [onKernelError],
-  );
+  }, [getVideoElement, flvPlayer, onFlvError]);
 
   React.useEffect(() => {
-    if (!flvPlayer) {
-      return () => {};
-    }
-    flvPlayer.on(flvjs.Events.ERROR, onError);
-    return () => {
-      try {
-        flvPlayer.off(flvjs.Events.ERROR);
-      } catch (errMsg) {}
-    };
-  }, [flvPlayer, onError]);
+    return () => setKernelMsg(null);
+  }, [src]);
 
   return kernelMsg;
 };
