acb21ab75a24ee14631f8e69abe42c95f5e81392
xiaosong <kunkkaco@gmail.com>
2019-08-16 18:15:47 +0800

    Updates

---
+++ precache-manifest.08961b82cfffb39906644523a778eb2c.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ece252644832d37f4c28aacaeb4fa9c8",
+    "url": "/react-player/index.html"
+  },
+  {
+    "revision": "c83f303bba80838cca08",
+    "url": "/react-player/static/css/2.9773bfdb.chunk.css"
+  },
+  {
+    "revision": "e74e6c23259362f21a58",
+    "url": "/react-player/static/css/3.89aefe71.chunk.css"
+  },
+  {
+    "revision": "ea917564b07de30868d8",
+    "url": "/react-player/static/css/main.7e4a267b.chunk.css"
+  },
+  {
+    "revision": "c83f303bba80838cca08",
+    "url": "/react-player/static/js/2.1981849c.chunk.js"
+  },
+  {
+    "revision": "e74e6c23259362f21a58",
+    "url": "/react-player/static/js/3.ef10dc4c.chunk.js"
+  },
+  {
+    "revision": "ea917564b07de30868d8",
+    "url": "/react-player/static/js/main.827fce64.chunk.js"
+  },
+  {
+    "revision": "9134cf8daa037e0f1edd",
+    "url": "/react-player/static/js/runtime~main.6a4b327c.js"
+  },
+  {
+    "revision": "4a087377ed1d4ad34b2d41e06196f557",
+    "url": "/react-player/static/media/GrindPlayer.4a087377.swf"
+  },
+  {
+    "revision": "f801e17431a5d3d0c7b9b1cbaa68f456",
+    "url": "/react-player/static/media/flashlsOSMF.f801e174.swf"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/react-player/precache-manifest.c68a7d1e8c1770f606526b896b702168.js"
+  "/react-player/precache-manifest.08961b82cfffb39906644523a778eb2c.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -35,5 +35,5 @@ workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
 workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/react-player/index.html"), {
   
-  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
