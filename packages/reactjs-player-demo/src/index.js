cb744aa490b57a82f9c57f869ad57fb018e223e4
xiaosong <kunkkaco@gmail.com>
2020-01-02 14:50:45 +0800

    feat: demo 增加 vconsole，方便手机上查看 log

--- packages/reactjs-player-demo/src/index.js
+++ packages/reactjs-player-demo/src/index.js
@@ -2,9 +2,12 @@ import 'react-app-polyfill/ie11';
 import 'react-app-polyfill/stable';
 import React from 'react';
 import ReactDOM from 'react-dom';
+import VConsole from 'vconsole';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+
+new VConsole();
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
