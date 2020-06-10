0128951ac34a319233430d6398fbaa6d44cd5856
xiaosong <kunkkaco@gmail.com>
2020-06-10 11:43:20 +0800

    fix: 修复 IE11 脚本错误

--- packages/use-autoplay/lib/use-autoplay.js
+++ packages/use-autoplay/lib/use-autoplay.js
@@ -15,16 +15,21 @@ export default (src, getVideoElement, prevented) => {
       return;
     }
     document.removeEventListener('click', onDocumentClick);
+    if (ref.current.prevented) {
+      el.play();
+      ref.current.loaded = true;
+      return;
+    }
     if (ref.current.loaded) {
       return;
     }
     ref.current.loaded = true;
     if (!ref.current.src) {
       el.src = '';
-      el.play().catch(() => {});
+      try {
+        el.play();
+      } catch (e) {}
       el.pause();
-    } else if (ref.current.prevented) {
-      el.play();
     }
   }, []);
 
